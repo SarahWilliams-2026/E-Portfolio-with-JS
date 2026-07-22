7a06ad48ce372a45d067bbcf41c6ce536f47477b
Sarahhassweetcheeks <Sarahhassweetcheeks@gmail.com>
2026-07-22 18:15:05 -0400

    Finished email js skeleton

--- app.js
+++ app.js
@@ -4,13 +4,27 @@
 
 function contact(event) {
     event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible"
+
     emailjs
         .sendForm(
-            'service_e92nala',
-            'template_hwm3c6p',
-            event.target,
-            'Me9GJA-jWLlDpEFQD'
+            "service_e92nala",
+            "template_hwm3c6p",
+            event.target
         ).then(() => {
-            console.log('this worked1')
-        })
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly at Sarahhassweetcheeks@gmail.com
+            );
+        });
+
+    setTimeout(() => {
+    
+        console.log('it worked 1')
+    }, 1000);
 }
