6788a901a2122a2ad37fabe0c77d594d3c84f8c6
Sarahhassweetcheeks <Sarahhassweetcheeks@gmail.com>
2026-07-22 21:10:29 -0400

    finished modals

--- app.js
+++ app.js
@@ -7,24 +7,28 @@ function contact(event) {
     const loading = document.querySelector('.modal__overlay--loading')
     const success = document.querySelector('.modal__overlay--success')
     loading.classList += " modal__overlay--visible"
-
     emailjs
         .sendForm(
             "service_e92nala",
             "template_hwm3c6p",
             event.target
-        ).then(() => {
-            loading.classList.remove("modal__overlay--visible");
-            success.classList += " modal__overlay--visible";
-        }).catch(() => {
-            loading.classList.remove("modal__overlay--visible");
-            alert(
-                "The email service is temporarily unavailable. Please contact me directly at Sarahhassweetcheeks@gmail.com
-            );
-        });
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly at Sarahhassweetcheeks@gmail.com"
+        );
+    });
+}
 
-    setTimeout(() => {
-    
-        console.log('it worked 1')
-    }, 1000);
+let isModalOpen = false;
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
 }
